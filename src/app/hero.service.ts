a3355327b10a5c243dc8dbf5956d29637b242ed6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-10-26 10:17:28 -0500

    [+] HeroService: Obtiene Datos del mock-heroes.js [+] HeroComponent: Vincula datos desde el HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HEROES } from './data/mock-heroes';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,8 @@ import { Injectable } from '@angular/core';
 export class HeroService {
 
   constructor() { }
+
+  getHeroes() {
+    return HEROES;
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../interfaces/hero';
+import { HeroService } from '../hero.service';
 
 import { HEROES } from '../data/mock-heroes';
 
@@ -8,14 +10,22 @@ import { HEROES } from '../data/mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-
-
-  public heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  public heroes: Hero[] = [];
 
   public selectedHero!: Hero;
 
+  constructor( private heroService: HeroService ) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect( hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
