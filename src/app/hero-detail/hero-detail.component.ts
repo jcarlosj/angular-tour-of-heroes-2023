99cd3756387475f36990a3bdc4edeabbc2450c56
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-10-26 12:23:56 -0500

    [+] AppRoutingModule: /detail/:id [+] HeroesComponent: routerLink [+] HeroDetailComponent: Recupera el valor del parametro pasado por la URL

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from '../interfaces/hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,5 +11,24 @@ import { Hero } from '../interfaces/hero';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent {
-  @Input() hero!: Hero;
+  public hero!: Hero;
+  id!: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    this.id = Number( this.route.snapshot.paramMap.get( 'id' ) );
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
