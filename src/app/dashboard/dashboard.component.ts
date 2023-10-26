997dd34d41bea6791a2137998d43211409d9db05
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-10-26 11:27:47 -0500

    [+] DashboardComponent: Obtener 5 primeros Heroes [+] AppRoutingModule: /dashboard, / [+] AppComponent: routerLink

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from '../interfaces/hero';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  heroes: Hero[] = [];
 
+  constructor( private heroService: HeroService ) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe( heroes => this.heroes = heroes.slice( 1, 5 ) );
+  }
 }
